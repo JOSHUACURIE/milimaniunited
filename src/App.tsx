432b9de845947f1fa27993f6bc906fe8b1374d03
JOSHUACURIE <joshuajuma039@gmail.com>
2025-11-07 20:53:36 +0300

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
-import Footer from './components/layout/Footer'; // Make sure this import exists
+import Footer from './components/layout/Footer';
 import Home from './pages/index';
 import './App.css';
 import WhoWeAre from './pages/whoweare';
@@ -19,6 +18,7 @@ import Newsletters from './pages/NewsLetters';
 import AlbumGallery from './pages/album';
 import Donate from './pages/donate';
 import PaymentSuccess from './components/payments/paymentsuccess';
+
 function App() {
   return (
     <Router>
@@ -29,19 +29,21 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about/who-we-are" element={<WhoWeAre />} />
             <Route path="/about/our-journey" element={<OurJourney />} />
-          <Route path="/about/vision-mission" element={<VisionMissionSpirit />} />
-          <Route path="/governance/board" element={<BoardOfDirectors />} />
-          <Route path="/governance/management" element={<SeniorManagement />} />
-          <Route path="/media/videos" element={<Videos />} />
-<Route path="/media/gallery" element={<Photos />} />
-<Route path="/knowledge/news" element={<NewsAndStories />} />
-<Route path="/knowledge/publications" element={<Publications />} />
-<Route path="/knowledge/reports" element={<Reports />} />
-<Route path="/knowledge/newsletters" element={<Newsletters />} />
-<Route path="/album" element={<AlbumGallery />} />
- <Route path="/donate" element={<Donate />} />
-        <Route path="/payment-success" element={< PaymentSuccess />} />
-<Route path="/knowledge/events" element={<Events />} />
+            <Route path="/about/vision-mission" element={<VisionMissionSpirit />} />
+            <Route path="/governance/board" element={<BoardOfDirectors />} />
+            <Route path="/governance/management" element={<SeniorManagement />} />
+            <Route path="/media/videos" element={<Videos />} />
+            <Route path="/media/gallery" element={<Photos />} />
+            <Route path="/knowledge/news" element={<NewsAndStories />} />
+            <Route path="/knowledge/publications" element={<Publications />} />
+            <Route path="/knowledge/reports" element={<Reports />} />
+            <Route path="/knowledge/newsletters" element={<Newsletters />} />
+            <Route path="/album" element={<AlbumGallery />} />
+            <Route path="/donate" element={<Donate />} />
+            <Route path="/payment-success" element={<PaymentSuccess />} />
+            <Route path="/knowledge/events" element={<Events />} />
+            {/* Add catch-all route for 404 pages */}
+            <Route path="*" element={<Home />} />
           </Routes>
         </main>
         <Footer /> 
